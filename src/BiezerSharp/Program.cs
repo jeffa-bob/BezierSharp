d2cb90aeb9ca09afef505bc5c805cd30ba635b7e
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-10 11:52:27 -0700

    initizalied

---
+++ src/BiezerSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SkiaSharp;
+
+namespace BiezerSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SKImageInfo imageInfo = new SKImageInfo(width: 640, height: 480, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
+            using (var surface = SKSurface.Create(imageInfo))
+            {
+                SKCanvas myCanvas = surface.Canvas;
+
+                // Your drawing code goes here.
+            }
+        }
+    }
+}
