9e86c545e1ad9f6d82a825376c1828fc1c9615a5
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-11 10:39:03 -0700

    added more things

--- src/BiezerSharp/Program.cs
+++ src/BiezerSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SkiaSharp;
+using SkiaSharp.Views;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Windowing.Desktop;
@@ -10,8 +11,33 @@ namespace BezierSharp
     {
         static void Main(string[] args)
         {
-            MainWindow window = new MainWindow(1080, 1920);
-            window.Run();
+            var p = new NativeWindowSettings();
+            
+            NativeWindow window = new NativeWindow(NativeWindowSettings.Default);
+            window.Context.MakeCurrent();
+            // window.MakeCurrent();
+
+            var glInterface = GRGlInterface.Create();
+            var context = GRContext.CreateGl(glInterface);
+            
+            System.Diagnostics.Debug.Assert(context != null);
+
+            SKImageInfo imageInfo = new SKImageInfo(width: 640, height: 480, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
+            var surface = SKSurface.Create(context, false, imageInfo);
+
+            //System.Diagnostics.Debug.Assert(surface != null);
+            SKCanvas canvas = surface.Canvas;
+
+            // Your drawing code goes here.
+            canvas.Clear(SKColors.White);
+            using (SKPaint paint = new SKPaint { Color = SKColors.AliceBlue })
+            {
+                canvas.DrawCircle(60, 90, 60, paint);
+            }
+
+            canvas.Flush();
+            context.Flush();
+
         }
     }
 }
