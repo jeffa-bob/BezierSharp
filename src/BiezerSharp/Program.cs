2c841ae85f33d50d9cea88fecc02fab3f702e519
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-10 18:57:28 -0700

    more things

--- src/BiezerSharp/Program.cs
+++ src/BiezerSharp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Desktop;
 
 namespace BiezerSharp
 {
@@ -8,12 +12,22 @@ namespace BiezerSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            GameWindow window = new GameWindow(GameWindowSettings.Default, NativeWindowSettings.Default);
+            // NativeWindow window = new NativeWindow(NativeWindowSettings.Default);
+            window.Run();
+            window.MakeCurrent();
+
+            GRContext context = GRContext.CreateGl();
+
             SKImageInfo imageInfo = new SKImageInfo(width: 640, height: 480, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
-            using (var surface = SKSurface.Create(imageInfo))
+            using (var surface = SKSurface.Create(context,false, imageInfo))
             {
-                SKCanvas myCanvas = surface.Canvas;
+                SKCanvas canvas = surface.Canvas;
 
                 // Your drawing code goes here.
+                canvas.Clear(SKColors.White);
+                canvas.Flush();
             }
         }
     }
