9e18d2a08b412e90e26845f6158f8051c84a2278
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-11 13:37:35 -0700

    fixed everything

---
+++ src/BezierSharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SkiaSharp;
+using GLFW;
+
+namespace BezierSharp2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var window = new NativeWindow(800, 600, "MyWindowTitle");
+        Glfw.MakeContextCurrent(window);
+      GRGlInterface glInterface = GRGlInterface.AssembleGlInterface(Glfw.CurrentContext, (contextHandle, name) => Glfw.GetProcAddress(name));
+      GRContext context = GRContext.CreateGl(glInterface);
+      GRBackendRenderTargetDesc backendRenderTargetDescription = new GRBackendRenderTargetDesc
+      {
+        Config = GRPixelConfig.Rgba8888,
+        Height = 300,
+        Width = 300,
+        Origin = GRSurfaceOrigin.TopLeft,
+        RenderTargetHandle = new IntPtr(0),
+        SampleCount = 0,
+        StencilBits = 8
+      };
+
+      using (var surface = SKSurface.Create(context, backendRenderTargetDescription))
+      {
+        var canvas = surface.Canvas;
+        // Main application loop
+        while (!window.IsClosing)
+        {
+          // OpenGL rendering
+          // Implement any timing for flow control, etc (see Glfw.GetTime())
+          canvas.Clear(SKColors.Aqua);
+          canvas.Flush();
+          context.Flush();
+          // Swap the front/back buffers
+          window.SwapBuffers();
+
+          // Poll native operating system events (must be called or OS will think application is hanging)
+          Glfw.PollEvents();
+        }
+      }
+
+    }
+  }
+}
