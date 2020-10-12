20c6e71b673ff3686ed0e72f3bc77e1ccedffbd8
jeffa-bob <brody.tyler.46@gmail.com>
2020-10-12 12:57:24 -0700

    added dragging

---
+++ src/BezierSharp/Mouse.cs
@@ -0,0 +1,74 @@
+using SkiaSharp;
+using System.Linq;
+using GLFW;
+using System;
+
+namespace BezierSharp2
+{
+  partial class Program
+  {
+    static bool LeftMouseIsHeld = false;
+    static bool holdingpiece;
+    static public bool isTouch;
+    static BezierPiece selected;
+
+    public static bool IsInsidePoint(SKPoint MousePoint, SKPoint point, int radius)
+    {
+      var absX = (MousePoint.X - point.X);
+      var absY = (MousePoint.Y - point.Y);
+      Console.WriteLine(((absX*absX)+(absY*absY)).ToString()+" "+(radius*radius).ToString());
+      return (absX*absX) + (absY*absY) <= radius * radius;
+    }
+    public static bool findpoint(SKPoint MousePoint,ref  BezierCurve curve)
+    {
+      foreach (BezierPiece piece in curve)
+      {
+        if (IsInsidePoint(MousePoint, piece.touch, 30))
+        {
+          isTouch = true;
+          selected = piece;
+          return true;
+        }
+        if (IsInsidePoint(MousePoint, piece.intersect, 30))
+        {
+          isTouch = false;
+          selected = piece;
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public static void AjustCurve(ref BezierCurve curve, int surfacehieght)
+    {
+      InputState a = Glfw.GetMouseButton(window, MouseButton.Left);
+      switch (a)
+      {
+        case InputState.Press:
+
+          double x, y;
+          Glfw.GetCursorPosition(window, out x, out y);
+          y = -y + surfacehieght;
+
+          if (!LeftMouseIsHeld)
+            holdingpiece = findpoint(new SKPoint((float)x, (float)y),ref curve);
+
+
+          if (holdingpiece)
+            if (isTouch)
+              selected.touch = new SKPoint((float)x, (float)y);
+            else
+              selected.intersect = new SKPoint((float)x, (float)y);
+
+          Console.WriteLine(x.ToString()+" "+y.ToString()+" "+ holdingpiece.ToString()+" "+LeftMouseIsHeld+" "+curve.Count);
+          LeftMouseIsHeld = true;
+          break;
+
+        case InputState.Release:
+          holdingpiece = false;
+          LeftMouseIsHeld = false;
+          break;
+      }
+    }
+  }
+}
--- src/BezierSharp/Program.cs
+++ src/BezierSharp/Program.cs
@@ -4,60 +4,66 @@ using GLFW;
 
 namespace BezierSharp2
 {
-    class Program
+  partial class Program
+  {
+
+    static BezierCurve a = new BezierCurve();
+    static NativeWindow window;
+    static void Main(string[] args)
     {
 
-        static void Main(string[] args)
+      a.Add(new BezierPiece(new SKPoint(80, 80), new SKPoint(-80, 150)));
+      a.Add(new BezierPiece(new SKPoint(280, 80), new SKPoint(380, 180)));
+      a.Add(new BezierPiece(new SKPoint(680, 80), new SKPoint(580, 10)));
+
+      using ( window = new NativeWindow(800, 600, "BezierSharp", Monitor.None, Window.None))
+      {
+        Glfw.MakeContextCurrent(window);
+        GRGlInterface glInterface = GRGlInterface.AssembleGlInterface(Glfw.CurrentContext, (contextHandle, name) => Glfw.GetProcAddress(name));
+        GRContext context = GRContext.CreateGl(glInterface);
+        GRBackendRenderTargetDesc backendRenderTargetDescription = new GRBackendRenderTargetDesc
         {
-            using (var window = new NativeWindow(800, 600, "BezierSharp", Monitor.None, Window.None))
+          Config = GRPixelConfig.Rgba8888,
+          Height = 600,
+          Width = 800,
+          Origin = GRSurfaceOrigin.TopLeft,
+          RenderTargetHandle = new IntPtr(0),
+          SampleCount = 0,
+          StencilBits = 8
+        };
+        
+
+        using (var surface = SKSurface.Create(context, backendRenderTargetDescription))
+        {
+          var canvas = surface.Canvas;
+          int height = canvas.DeviceClipBounds.Height;
+          // Main application loop
+          while (!window.IsClosing)
+          {
+            AjustCurve(ref a, height);
+            canvas.Clear(SKColors.White);
+            using (var paint = new SKPaint { Color = SKColors.Blue })
             {
-                Glfw.MakeContextCurrent(window);
-                GRGlInterface glInterface = GRGlInterface.AssembleGlInterface(Glfw.CurrentContext, (contextHandle, name) => Glfw.GetProcAddress(name));
-                GRContext context = GRContext.CreateGl(glInterface);
-                GRBackendRenderTargetDesc backendRenderTargetDescription = new GRBackendRenderTargetDesc
-                {
-                    Config = GRPixelConfig.Rgba8888,
-                    Height = 600,
-                    Width = 800,
-                    Origin = GRSurfaceOrigin.TopLeft,
-                    RenderTargetHandle = new IntPtr(0),
-                    SampleCount = 0,
-                    StencilBits = 8
-                };
+              paint.StrokeWidth = 3;
+              paint.Style = SKPaintStyle.Stroke;
+              paint.IsAntialias = true;
 
-                using (var surface = SKSurface.Create(context, backendRenderTargetDescription))
-                {
-                    var canvas = surface.Canvas;
-
-                    // Main application loop
-                    while (!window.IsClosing)
-                    {
-                        canvas.Clear(SKColors.White);
-                        using (var paint = new SKPaint { Color = SKColors.Blue })
-                        {
-                            paint.StrokeWidth = 3;
-                            paint.Style = SKPaintStyle.Stroke;
-                            paint.IsAntialias = true;
-
-                            var a = new BezierCurve();
-                            a.Add(new BezierPiece(new SKPoint(80, 80), new SKPoint(-80, 150)));
-                            a.Add(new BezierPiece(new SKPoint(280, 80), new SKPoint(380, 180)));
-                            a.Add(new BezierPiece(new SKPoint(680, 80), new SKPoint(580, 10)));
-                            a.Draw(ref canvas, paint, SKPathFillType.Winding);
-                        }
-                        // OpenGL rendering
-                        // Implement any timing for flow control, etc (see Glfw.GetTime())
-                        canvas.Flush();
-                        context.Flush();
-                        // Swap the front/back buffers
-                        window.SwapBuffers();
-
-                        // Poll native operating system events (must be called or OS will think application is hanging)
-                        Glfw.PollEvents();
-                    }
-                }
-
+              a.Draw(ref canvas, paint, SKPathFillType.Winding);
             }
+            
+            // OpenGL rendering
+            // Implement any timing for flow control, etc (see Glfw.GetTime())
+            canvas.Flush();
+            context.Flush();
+            // Swap the front/back buffers
+            window.SwapBuffers();
+
+            // Poll native operating system events (must be called or OS will think application is hanging)
+            Glfw.PollEvents();
+          }
         }
+
+      }
     }
+  }
 }
